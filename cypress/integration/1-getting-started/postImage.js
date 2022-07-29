30f966bb2147a0ea5e23a8fc2d14b65c6e3a01e4
Sonu <sonoo62p@gmail.com>
2022-07-28 20:43:07 -0400

    postimage

---
+++ cypress/integration/1-getting-started/postImage.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe("Post image as Artist", () => {
+  beforeEach(() => {
+    cy.visit(Cypress.config("baseUrl"));
+  })
+
+
+  it('UploadPost ', () => {
+  })
+
+
+})
