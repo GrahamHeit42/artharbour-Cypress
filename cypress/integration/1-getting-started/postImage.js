3659d2b844ccd72be45524f1b03c3be31ebb6a61
Sonu <sonoo62p@gmail.com>
2022-08-18 16:50:31 -0400

    delete extra folder

--- cypress/integration/1-getting-started/postImage.js
+++ cypress/integration/1-getting-started/postImage.js
@@ -2,52 +2,62 @@
 
 describe("Post image", () => {
   beforeEach(() => {
-    cy.LoginSession('Dhruv@gmail.com', '12345678');
+    cy.LoginSession('dhruv@gmail.com', '12345678');
     cy.visit(Cypress.config('baseUrl'))
+    cy.wait(2000);
   })
 
 
   it('Upload image as artist', () => {
 
-    cy.get('[src="assets/icons/plus.png"]').click()
-    //cy.get('.sm:col-span-2').click()
-    // cy.get('.mat-focus-indicator w-full mat-flat-button mat-button-base mat-primary').contains('primary').click()
-    cy.get('button').contains('Artist').click();
-    cy.get('button').contains('Personal').click();
+    // cy.get('[src="assets/icons/plus.png"]').click()
+    // cy.get('button').contains('Artist').click();
+    // cy.wait(1000);
+    // cy.get('button').contains('Personal').click();
+    // cy.wait(1000);
+    // cy.get('[placeholder="Title *"]').click({ force: true }).type('blue', { force: true });
+    // cy.wait(2000);
+    // cy.get('[placeholder="Description *"]').click({ force: true }).type('only for testing', { force: true });
+    // cy.wait(1000);
+    // cy.get('mat-select').contains('Select Subject *').click();
+    // cy.get('.mat-option-text').contains('Comic').click();
+    // cy.wait(1000);
+    // cy.get('mat-select').contains('Select Medium *').click();
+    // cy.get('.mat-option-text').contains('Digital 2D').click();
+    // cy.get('[placeholder="Add New Tag..."]').click({ force: true }).type('test', { force: true });
+    // cy.wait(1000);
+    // cy.get('.mat-checkbox-inner-container').click({ force: true });
+    // cy.wait(1000);
+    // cy.get('.mat-radio-label-content').contains('Mature ').click();
+    // cy.wait(1000);
 
-    //cy.get('[name*="Title"]').type('test')
-    //cy.get('[name="Title"]').type('blue')
-    //cy.get('[placeholder="Title *"]').type('test')
-    //cy.get("#mat-input-2").type('Blue')
-    //cy.get('.mat-form-field-label-wrapper ng-tns-c36-19').type('Blue')
-
-    cy.get('[placeholder="Title *"]').click({ force: true }).type('blue', { force: true });
-
-    cy.get('[placeholder="Description *"]').click({ force: true }).type('only for testing', { force: true });
-    cy.get('mat-select').contains('Select Subject *').click();
-    cy.get('mat-option').contains('Anime/Manga').click();
-    //cy.get('[placeholder="Description *"]').click({ force: true }).type('for testing');
-    // cy.get("#mat-input-5").click({ force: true }).type('for testing');
-    //cy.get('Select Medium').contains('traditional paint').click();
-    //cy.get('mat-medium').contains('mixed media').click();
-    cy.get('mat-select').contains('Select Medium *').click();
-    cy.get('mat-option').contains('mixed media').click();
-    cy.get('[placeholder = "Add New Tag..."]').type('blue,black');
-    //cy.get('.mat-checkbox-label').click();
-    //cy.get('.mat-checkbox-inner-container').dblclick();
-
-    //cy.findByRole(".mat-checkbox-inner-container").check().should("be.checked");
-    //cy.get(".accordion__itemContent").invoke("display").equal("block")
-    cy.get('.mat-checkbox-label').click({ force: true });
-    //cy.get('mat-radio-button').contains('mat-radio-4-input').click({ force: true });
-    //cy.get('mat-radio-button').contains('mat-radio-outer-circle').click({ force: true });
-    cy.get('.mat-radio-label').contains('Mature').click();
-    //cy.get('.mat-button-wrapper').contains(' Upload ').click({ force: true });
-    cy.get('.mat-button-wrapper').contains(' Upload ').click({ force: true });
-    //cy.get('Drop files here').contains('.ng-star-inserted').click({ force: true });
-    cy.get('.drop-area-class').contains('.drop-content-class').click();
-    //cy.get('.drop-area-class').contains('ngx-file-drop__file-input').click({ force: true });
   })
-
-
+  it('Upload image as Commissioned', () => {
+    cy.get('[src="assets/icons/plus.png"]').click()
+    cy.get('button').contains('Artist').click();
+    cy.wait(2000);
+    cy.get('.mat-button-wrapper').contains('Commissioned').click();
+    cy.wait(1000);
+    cy.get('[placeholder="Title *"]').click({ force: true }).type('testing for commissioned', { force: true });
+    cy.wait(1000);
+    cy.get('[placeholder="Description *"]').click({ force: true }).type('for new commissioned', { force: true });
+    cy.wait(1000);
+    cy.get('[data-placeholder="Commissioned by"]').click({ force: true }).type('sonu rathod', { force: true });
+    cy.wait(1000);
+    cy.get('mat-select').contains('Select Subject *').click();
+    cy.get('.mat-option-text').contains('Fantasy').click();
+    cy.wait(1000);
+    cy.get('mat-select').contains('Select Medium *').click();
+    cy.get('.mat-option-text').contains('Mixed Media').click();
+    cy.wait(1000);
+    cy.get('[placeholder="Add New Tag..."]').click({ force: true }).type('magic', { force: true });
+    cy.get('mat-icon').contains('star_border').click();
+    cy.get('mat-icon').contains('star_border').click();
+    cy.get('mat-icon').contains('star_border').click();
+    cy.get('mat-icon').contains('star_border').click();
+    cy.get('mat-icon').contains('star_border').click();
+    cy.get('mat-icon').contains('star_border').click();
+    cy.get('mat-icon').contains('star_border').click();
+    cy.get('mat-icon').contains('star_border').click();
+  })
 })
